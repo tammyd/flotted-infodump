4215d0f367b5b5aa71c160956b47e22ac3e7fae5
Tammy D <tammyd@gmail.com>
2012-12-24 09:12:38 -0800

    Removed test data

--- src/Mefi/InfoDumpBundle/Controller/UserInfoController.php
+++ src/Mefi/InfoDumpBundle/Controller/UserInfoController.php
@@ -10,20 +10,6 @@ use Symfony\Component\HttpFoundation\Response;
 
 class UserInfoController extends Controller
 {
-    public function testDataAction()
-    {
-
-        //return $this->render('MefiInfoDumpBundle:UserInfo:test.html.twig', array('test'=>'This is a test'));
-        $response = new Response(json_encode(array('d1' => array(1,2,3,4))));
-        $response->headers->set('Content-Type', 'application/json');
-
-        return $response;
-    }
-
-    public function testContentAction()
-    {
-        return $this->render('MefiInfoDumpBundle:UserInfo:test.html.twig');
-    }
 
     public function signupsByDateDataAction() {
 
---
+++ src/Mefi/InfoDumpBundle/Resources/public/js/graphHelpers.js
@@ -0,0 +1,27 @@
+var graphHelpers = (function() {
+
+    return {
+        showTooltip: function(x, y, contents) {
+            $('<div id="tooltip">' + contents + '</div>').css( {
+                position: 'absolute',
+                display: 'none',
+                top: y + 5,
+                left: x + 5,
+                border: '1px solid #fdd',
+                padding: '2px',
+                'background-color': '#fee',
+                opacity: 0.80
+            }).appendTo("body").fadeIn(200);
+        },
+        graph: function() {
+            return $("#graph");
+        },
+        getMonthAbbr: function(i) {
+            var months = {
+                1:'Jan', 2:'Feb', 3:'Mar', 4:'Apr', 5:'May', 6:'Jun',
+                7:'July', 8:'Oct', 9:'Sept', 10:'Oct', 11:'Nov', 12:'Dec'
+            }
+            return months[i];
+        }
+    }
+})();
