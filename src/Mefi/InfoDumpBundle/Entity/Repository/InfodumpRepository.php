20da861e7683150a4b073af48c04898de3262970
Tammy D <tammyd@gmail.com>
2012-12-29 22:59:29 -0800

    Using memcache for result set caching.

--- src/Mefi/InfoDumpBundle/Entity/Repository/InfodumpRepository.php
+++ src/Mefi/InfoDumpBundle/Entity/Repository/InfodumpRepository.php
@@ -7,8 +7,11 @@ use  Doctrine\ORM\Query\ResultSetMapping;
 
 abstract class InfodumpRepository extends EntityRepository
 {
+    protected function getCachePrefix() {
+        return $this->getClassMetadata()->getTableName();
+    }
 
-    public function getCountByDate($dateField)
+    public function getCountByDate($dateField, $cache=true, $cacheTime=3600)
     {
         $table = $this->getClassMetadata()->getTableName();
         $sql = "select count(*) as count, date($dateField) as date from $table group by date order by date asc";
@@ -16,13 +19,16 @@ abstract class InfodumpRepository extends EntityRepository
         $rsm = new ResultSetMapping();
         $rsm->addScalarResult('date', 'date', 'string');
         $rsm->addScalarResult('count', 'count', 'integer');
+
+        $cacheKey = $this->buildCacheKey(__FUNCTION__, $dateField);
         return $this->getEntityManager()
             ->createNativeQuery($sql, $rsm)
+            ->useResultCache($cache, $cacheTime, $cacheKey)
             ->getResult();
 
     }
 
-    public function getCountByMonthYear($dateField)
+    public function getCountByMonthYear($dateField, $cache=true, $cacheTime=3600)
     {
         $table = $this->getClassMetadata()->getTableName();
         $sql = <<<SQL
@@ -37,12 +43,14 @@ SQL;
         $rsm->addScalarResult('month', 'month', 'integer');
         $rsm->addScalarResult('year', 'year', 'integer');
 
+        $cacheKey = $this->buildCacheKey(__FUNCTION__, $dateField);
         return $this->getEntityManager()
             ->createNativeQuery($sql, $rsm)
+            ->useResultCache($cache, $cacheTime, $cacheKey)
             ->getResult();
     }
 
-    public function getCountByYear($dateField)
+    public function getCountByYear($dateField, $cache=true, $cacheTime=3600)
     {
         $table = $this->getClassMetadata()->getTableName();
         $sql = "select count(*) as count, year($dateField) as date from $table group by date order by date asc";
@@ -51,12 +59,14 @@ SQL;
         $rsm->addScalarResult('date', 'date', 'integer');
         $rsm->addScalarResult('count', 'count', 'integer');
 
+        $cacheKey = $this->buildCacheKey(__FUNCTION__, $dateField);
         return $this->getEntityManager()
             ->createNativeQuery($sql, $rsm)
+            ->useResultCache($cache, $cacheTime, $cacheKey)
             ->getResult();
     }
 
-    public function getCountByYearDayOfWeek($dateField)
+    public function getCountByYearDayOfWeek($dateField, $cache=true, $cacheTime=3600)
     {
         $table = $this->getClassMetadata()->getTableName();
         $rsm = new ResultSetMapping();
@@ -65,23 +75,32 @@ SQL;
         $rsm->addScalarResult('year', 'year', 'integer');
         $sql = "SELECT COUNT( * ) AS count, year($dateField) as year, DAYOFWEEK( $dateField ) AS dow FROM $table GROUP BY year,dow ORDER BY year asc, dow ASC ";
 
+        $cacheKey = $this->buildCacheKey(__FUNCTION__, $dateField);
         $result = $this->getEntityManager()
             ->createNativeQuery($sql, $rsm)
+            ->useResultCache($cache, $cacheTime, $cacheKey)
             ->getResult();
         return $result;
     }
 
-    public function getCountByHour($datefield)
+    public function getCountByHour($datefield, $cache=true, $cacheTime=3600)
     {
         $table = $this->getClassMetadata()->getTableName();
         $rsm = new ResultSetMapping();
         $rsm->addScalarResult('hour', 'hour', 'integer');
         $rsm->addScalarResult('count', 'count', 'integer');
         $sql = "select count(*) as count, hour($datefield) as hour from $table group by hour order by hour asc;";
+
+        $cacheKey = $this->buildCacheKey(__FUNCTION__, $datefield);
         $result = $this->getEntityManager()
             ->createNativeQuery($sql, $rsm)
+            ->useResultCache($cache, $cacheTime, $cacheKey)
             ->getResult();
         return $result;
+    }
+
+    private function buildCacheKey($function, $args) {
+        return $this->getCachePrefix().":".$function.":".md5(json_encode($args));
     }
 
 }
