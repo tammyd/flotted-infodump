2c2dbb1663b36a2f4307d176c9fee0448b1d28f4
Tammy D <tammyd@gmail.com>
2013-01-07 07:30:55 -0800

    Removed now-unused Entity repositories

--- src/Mefi/InfoDumpBundle/Entity/CommentdataAskme.php
+++ src/Mefi/InfoDumpBundle/Entity/CommentdataAskme.php
@@ -8,7 +8,6 @@ use Doctrine\ORM\Mapping as ORM;
  * Mefi\InfoDumpBundle\Entity\CommentdataAskme
  *
  * @ORM\Table(name="commentdata_askme")
- * @ORM\Entity(repositoryClass="Mefi\InfoDumpBundle\Entity\Repository\CommentdataAskmeRepository")
  */
 class CommentdataAskme
 {
--- src/Mefi/InfoDumpBundle/Entity/Usernames.php
+++ src/Mefi/InfoDumpBundle/Entity/Usernames.php
@@ -8,7 +8,6 @@ use Doctrine\ORM\Mapping as ORM;
  * Mefi\InfoDumpBundle\Entity\Usernames
  *
  * @ORM\Table(name="usernames")
- * @ORM\Entity(repositoryClass="Mefi\InfoDumpBundle\Entity\Repository\UsernamesRepository")
  */
 class Usernames
 {
