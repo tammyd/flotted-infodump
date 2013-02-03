90ad946cd87a1a4a66ef1e1ff00be74045d01805
Tammy D <tammyd@gmail.com>
2013-02-03 08:03:28 -0800

    Script to load in text data given MySQL's LOAD INFILE isn't permitted on my webhost.
    Refactored data.py for loading in db.
    
    DB load fix

--- src/Mefi/InfoDumpBundle/Resources/config/sql/data.py
+++ src/Mefi/InfoDumpBundle/Resources/config/sql/data.py
@@ -7,116 +7,210 @@ import subprocess
 import sys
 import math
 import time
-
-def runProcess(exe):
-    p = subprocess.Popen(exe, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    while(True):
-        retcode = p.poll() #returns None while subprocess is running
-        line = p.stdout.readline()
-        yield line
-        if(retcode is not None):
-            break
+import warnings
+import os
 
 
-def execSQL(cursor, sql):
-    try:
-        cursor.execute(sql)
-    except Exception:
-        print "Error with SQL:"
-        print sql
-        print
+class infodumpLoader:
+    debug = False
+    defaultOptions =  {'auto':False, 'max':400, 'skip':2, 'sanity':None, 'skip_check':False}
+
+    def __del__(self):
+        self.db.commit()
+        self.cursor.close()
+        self.db.close()
+
+    def __init__(self, dbname, user, password, host, directory, dataOptions, debug=False, defaults={}):
+
+        self.db = MySQLdb.connect(host,user,password,dbname)
+        self.cursor = self.db.cursor()
+        self.filedir = directory
+        self.fileOptions = dataOptions
+        self.debug = debug
+        if defaults:
+            self.defaultOptions = defaults
+
+    def run(self):
+        tables = []
+        for table in self.fileOptions:
+            tables.append(table)
+        tables.sort()
+        for table in tables:
+            self.loadTable(table)
+
+    def test(self, table, options):
+        savedOptions = self.fileOptions[table]
+        self.fileOptions[table] = options
+        self.loadTable(table)
+        self.fileOptions[table] = savedOptions
+
+    def _execSQL(self, sql, values=[]):
+        self.cursor.execute(sql, values)
+        #print self.cursor._executed
+
+#        if self.debug or True:
+#            try:
+#                with warnings.catch_warnings(record=True) as w:
+#                    warnings.simplefilter("always")
+#                    self.cursor.execute(sql, values)
+#                    if len(w):
+#                        print (w[0].message)
+#                        raise Exception("SQL warning ")
+#            except Exception:
+#                print "Error with SQL stmt (%d values): %s:" % (len(values), sql)
+#        else:
+#            try:
+#                self.cursor.execute(sql, values)
+#            except Exception:
+#                print "Error with SQL stmt (%d values): %s:" % (len(values), sql)
+##        sys.stderr.write(sql + "\n")
+##        sys.stderr.write(','.join(values))
+##        sys.stderr.write("\n")
 
 
-def linecount(filename):
-    result = runProcess(['wc', '-l', filename])
-    line = result.next()
-    result = line.strip().split(' ')[0]
-    return int(result)
+    def _runProcess(self, exe):
+        p = subprocess.Popen(exe, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        while(True):
+            retcode = p.poll() #returns None while subprocess is running
+            line = p.stdout.readline()
+            yield line
+            if(retcode is not None):
+                break
 
-def tablecount(table, dbCursor):
-    execSQL(dbCursor, "SELECT count(*) from %s" % table)
-    count = dbCursor.fetchone()[0]
-    return count
+    def _linecount(self, filename):
+        result = self._runProcess(['wc', '-l', filename])
+        line = result.next()
+        result = line.strip().split(' ')[0]
+        return int(result)
 
-def checkLoad(filename, table, dbCursor, skipRows=2):
-    entries = linecount(filename) - skipRows
-    count = tablecount(table, dbCursor)
-    return entries==count
+    def _tablecount(self, table):
+        self._execSQL("SELECT count(*) from %s" % table)
+        count = self.cursor.fetchone()[0]
+        return count
 
-
-def loadFile(filename, table, dbCursor, autoIncrement=False, skipRows=2, maxPerStmt=100):
-    startSQL = True
-    endSQL = False
-    sqlStmt = ""
-    recordCount = linecount(filename) - skipRows
-    percentMarked = 0
-
-    #check if we're already done...
-    ok = checkLoad(filename, table, cursor, skipRows)
-    if ok:
-        count = tablecount(table, cursor)
-        print "%s already loaded  with %d rows." % (table,count)
-        return
-
-    startTime = time.time()
-
-    dbCursor.execute("TRUNCATE table %s" % table)
-
-    print "Loading %s" % filename
-    with open(filename) as infile:
-        for i,line in enumerate(infile):
-            if i < skipRows:
-                continue
-
-            if i % maxPerStmt == 0:
-                endSQL = True
-
-            if (autoIncrement):
-                vals = 'null,"'+'","'.join(line.strip().split('\t'))+'"'
-            else:
-                vals = '"'+'","'.join(line.strip().split('\t'))+'"'
-
-            if startSQL:
-                sqlStmt = "INSERT into %s VALUES" % table
-                startSQL = False
-
-            sqlStmt = "%s (%s)" % (sqlStmt, vals)
-
-            if endSQL:
-                endSQL = False
-                sqlStmt += ";"
-                execSQL(cursor, sqlStmt)
-                startSQL = True
-                percentMarked = markPercent(i,skipRows, recordCount, percentMarked)
-            else:
-                sqlStmt += ","
-
-        sqlStmt = sqlStmt[0:-1] + ";"   #trim comma, add semi-colon
-        execSQL(cursor, sqlStmt)        #add last rows
-        percentMarked = markPercent(i+1,skipRows, recordCount, percentMarked)
-
-        ok = checkLoad(filename, table, cursor, skipRows)
-        count = tablecount(table, cursor)
-        if (ok):
-            print "%s loaded successfully with %d rows." % (table,count)
+    def getTableOption(self, table, key):
+        retval = None
+        if key in self.fileOptions[table]:
+            retval = self.fileOptions[table][key]
+        elif key in self.defaultOptions:
+            retval = self.defaultOptions[key]
         else:
-            print "Inconsistant load of %s. Expected %d results, got %d." % (table, recordCount, count)
-        print "Execution time: %0.02f sec" % (time.time()-startTime)
+            retval = None
 
-def markPercent(i, skipRows, recordCount, percentMarked):
-    percentDone = int(math.floor(100*(i-skipRows) / recordCount))
-    if (percentDone > percentMarked):
-        for i in range(percentMarked+1,percentDone+1):
-            if i % 20 == 0:
-                print "%3d%%" % i
-            elif i % 5 == 0:
-                print "%2d%%" % i,
+        return retval
+
+    def checkTable(self, table):
+        filename = self.getFilename(table)
+        skipRows = self.getTableOption(table, 'skip')
+        entries = self._linecount(filename) - skipRows
+        count = self._tablecount(table)
+        return entries==count
+
+    def getFilename(self, table):
+        return os.path.join(self.filedir, table + ".txt")
+
+    def loadTable(self, table):
+        filename = self.getFilename(table)
+        skipRows = self.getTableOption(table, 'skip')
+        sanityFtn = self.getTableOption(table, 'sanity')
+        maxPerStmt = self.getTableOption(table, 'max')
+        autoIncrement = self.getTableOption(table, 'auto')
+        fileRecords = self._linecount(filename) - skipRows
+        maxRows = self.getTableOption(table, 'max_rows')
+        percentMarked = 0
+        numberSkipped = 0
+        startSQL = True
+        endSQL = False
+        skipCheck = self.getTableOption(table, 'skip_check')
+        sqlStart = sqlPortion = ""
+
+        sqlStmt = ""
+        values = []
+
+        if not skipCheck and self.checkTable(table):
+            count = self._tablecount(table)
+            print "%s already loaded with %d rows." % (table,count)
+            return
+        elif skipCheck:
+            print "Skipping check of %s" % table
+
+
+        startTime = time.time()
+        self._execSQL("TRUNCATE table %s" % table)
+        print "Truncated %s " % table
+        print "Loading %s" % filename
+        with open(filename) as infile:
+            for i,line in enumerate(infile):
+                if i < skipRows:
+                    continue
+                if maxRows and i >= (maxRows+skipRows+numberSkipped):
+                    continue
+
+                lineValues = line.strip().split('\t')
+                if sanityFtn:
+                    lineValues = sanityFtn(lineValues)
+                    if lineValues is None:
+                        numberSkipped+=1
+                        sys.stderr.write('Sanity check failed for line %d of %s\n' % (i, filename))
+                        continue
+
+                if i % maxPerStmt == 0:
+                    endSQL = True
+
+                if startSQL:
+                    sqlStmt = "INSERT into %s VALUES " % table
+                    values = []
+                    startSQL = False
+
+                if autoIncrement:
+                    sqlStart = "(null, %"
+                else:
+                    sqlStart = "(%"
+
+                count = len(lineValues)-1
+                sqlPortion = sqlStart + "s, %"*count + "s)"
+
+                #ignore the unicode errors. Not sure what's going on w/ the db, but it's the best I can do.
+                uni = [ unicode(x,errors='ignore') for x in lineValues ]
+                values += uni
+
+                sqlStmt += sqlPortion
+                if endSQL:
+                    endSQL = False
+                    sqlStmt += ";"
+                    self._execSQL(sqlStmt, values)
+                    values = []
+                    startSQL = True
+                    percentMarked = self._markPercent(i,skipRows, fileRecords, percentMarked)
+                else:
+                    sqlStmt += ","
+
+            #finish up
+            sqlStmt = sqlStmt[0:-1] + ";"         #trim comma, add semi-colon
+            self._execSQL(sqlStmt, values)        #add last rows
+            percentMarked = self._markPercent(i+1,skipRows, fileRecords, percentMarked)
+
+            count = self._tablecount(table)
+            if self.checkTable(table):
+                print "%s loaded successfully with %d rows." % (table,count)
             else:
-                print '.',
+                print "Inconsistant load of %s. Expected %d results, got %d. (Skipped %d)." % (table, fileRecords, count, numberSkipped)
 
-        sys.stdout.flush()
-        percentMarked = percentDone
-    return percentMarked
+
+    def _markPercent(self, i, skipRows, recordCount, percentMarked):
+        percentDone = int(math.floor(100*(i-skipRows) / recordCount))
+        if (percentDone > percentMarked):
+            for i in range(percentMarked+1,percentDone+1):
+                if i % 20 == 0:
+                    print "%3d%%" % i
+                elif i % 5 == 0:
+                    print "%2d%%" % i,
+                else:
+                    print '.',
+
+            sys.stdout.flush()
+            percentMarked = percentDone
+        return percentMarked
 
 
 if __name__ == '__main__':
@@ -131,54 +225,82 @@ if __name__ == '__main__':
         print "Usage: %s <host> <user> <password> <dbname> <datadir>" % sys.argv[0]
         exit()
 
-    subsites = ['music','meta','askme','meta']
-    common = ['postdata', 'postlength','posttitles','tagdata',  'commentdata', 'commentlength']
-    others = ['favoritesdata', 'contactdata', 'usernames']
+    def defaultSanity(lineValues, length):
+    #If function returns False, then the line couldn't be saved.
+    #Otherwise, return the "fixed" value
+        if len(lineValues) != length:
+            return None
+        return lineValues
 
-    extras = {
-        'favoritesdata':{'auto':True},
-        'postdata_music':{'max':1}
+    def postDataSanity(lineValues):
+        if defaultSanity(lineValues, 8) is None:
+            return None
+        for i in range(3,7):
+            x = lineValues[i]
+            if isinstance(x, basestring):
+                if not x:
+                    return None
+        return lineValues
+
+    def favoritesSanity(lineValues):
+        if len(lineValues) != 7:
+            return None
+
+        return ['NULL'] + lineValues
+
+    def appendLastSanity(lineValues, length, missing):
+        if len(lineValues) == length:
+            return lineValues
+        if len(lineValues) == (length-1):
+            return lineValues + [missing]
+        else:
+            return None
+
+    def appendLastSanityWithIntTypeCheck(lineValues, length, missing):
+        if len(lineValues) == length:
+            return lineValues
+        if len(lineValues) == (length-1):
+            if isinstance(lineValues[length-2], (int, long) ):
+                return lineValues + [missing]
+
+        return None
+    
+    sites = {
+        'postdata_music': {'sanity': lambda x:appendLastSanityWithIntTypeCheck(x, 8, '[NULL]')},
+        'postdata_meta': {'sanity': lambda x:postDataSanity(x)},
+        'postdata_askme': {'sanity': lambda x:appendLastSanityWithIntTypeCheck(x, 8, '[NULL]')},
+        'postdata_mefi': {'sanity': lambda x:postDataSanity(x)},
+        'postlength_music': {},
+        'postlength_meta': {},
+        'postlength_askme': {},
+        'postlength_mefi': {},
+        'tagdata_music': {},
+        'tagdata_meta': {},
+        'tagdata_askme': {},
+        'tagdata_mefi': {},
+        'commentdata_music': {},
+        'commentdata_meta': {},
+        'commentdata_askme': {},
+        'commentdata_mefi': {},
+        'commentlength_music': {},
+        'commentlength_meta': {},
+        'commentlength_askme': {},
+        'commentlength_mefi': {},
+        'contactdata': {'auto':True},
+        'usernames': {},
+        'favoritesdata': {'auto':True},
+        'posttitles_music': {'sanity': lambda x:defaultSanity(x, 2)},
+        'posttitles_meta': {'sanity': lambda x:defaultSanity(x, 2)},
+        'posttitles_askme': {'sanity': lambda x:defaultSanity(x, 2)},
+        'posttitles_mefi': {'sanity': lambda x:defaultSanity(x, 2)},
     }
 
-    def getExtraTableVal(table, key):
-        retval = None
-        if table in extras:
-            if key in extras[table]:
-                retval = extras[table][key]
-        return retval
-
-    def loadFileWithOptions(filename, table, cursor):
-        auto = True if getExtraTableVal(table, 'auto') else False
-        if getExtraTableVal(table, 'max'):
-            loadFile(filename, table, cursor, auto, maxPerStmt=getExtraTableVal(table, 'max'))
-        else:
-            loadFile(filename, table, cursor, auto)
+    default = {'auto':False, 'max':400, 'skip':2, 'sanity':None, 'skip_check':False}
+    loader = infodumpLoader(dbname,user, password,host,dir,sites, defaults=default)
+    loader.run()
 
 
-    autoincrement = {'favoritesdata'}
-    maxPerStmt = {'postdata_music':1}
 
-    db = MySQLdb.connect(host,user,password,dbname)
-    cursor=db.cursor()
-
-    testTables = ['posttitles_music','posttitles_meta','posttitles_askme','posttitles_mefi']
-
-    if len(testTables) > 0:
-        for table in testTables:
-            filename = "%s/%s.txt" % (dir, table)
-            auto = True if getExtraTableVal(table, 'auto') else False
-            loadFile(filename, table, cursor, auto, maxPerStmt=1)
-        exit()
-
-    for site in subsites:
-        for section in common:
-            table = '%s_%s' % (section, site)
-            filename = "%s/%s.txt" % (dir, table)
-            loadFileWithOptions(filename, table, cursor)
-
-    for table in others:
-        filename = "%s/%s.txt" % (dir, table)
-        loadFileWithOptions(filename, table, cursor)
 
 
 
